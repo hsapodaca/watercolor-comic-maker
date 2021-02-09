97b2363ca745530dadb138a15314c1fe87d352c0
hsapodaca <svet.apodaca@gmail.com>
2021-02-08 21:04:20 -0800

    cleanup, add tests

--- hsapodaca/test/test_main.py
+++ hsapodaca/test/test_main.py
@@ -9,3 +9,29 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
+
+
+def test_read_comic():
+    response = client.get("/xkcd/100")
+    assert response.status_code == 200
+
+
+def test_read_comic_watercolor():
+    response = client.get("/xkcd/100/watercolor")
+    assert response.status_code == 200
+
+
+def test_read_random_comic():
+    response = client.get("/xkcd/random")
+    assert response.status_code == 200
+
+
+def test_read_random_comic_watercolor():
+    response = client.get("/xkcd/random/watercolor")
+    assert response.status_code == 200
+
+
+def test_read_comic_nonexistent():
+    response = client.get("/xkcd/100000000000")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Comic not found"}
