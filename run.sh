f5e31b40a7df746bc69ee2434670359f2d54206a
hsapodaca <svet.apodaca@gmail.com>
2021-02-08 21:05:57 -0800

    remove flask setup, add tests

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 echo "Installing required modules"
 pip install -r requirements.txt
 
-echo "Setting up Flask"
-export FLASK_ENV=development
-export FLASK_APP=app.py
+echo "Running tests"
+python -m pytest hsapodaca/test
 
 echo "Starting server on port 8888"
  uvicorn hsapodaca.src.main:app --host 0.0.0.0 --port 8888
